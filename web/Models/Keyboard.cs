4e8270274e3e909bb5028f264e1208f9918d7276
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-28 22:23:05 +0300

    add(KeyboardModel): id

--- web/Models/Keyboard.cs
+++ web/Models/Keyboard.cs
@@ -2,6 +2,7 @@ namespace web.Models
 {
     public class Keyboard
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Img { get; set; }
         public string Description { get; set; }
