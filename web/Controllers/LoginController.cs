00493058f1c15438a41d3780350f3bf14db81b5a
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-30 14:11:41 +0300

    add: login page

---
+++ web/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
